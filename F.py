5ef7ac23f4c839fc5294d59e12e3a04a0f1b47a3
Evgeniy <99466328+zhenya0914@users.noreply.github.com>
2022-02-11 20:04:37 +0600

    Add files via upload

---
+++ F.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = []
+i=int()
+
+if n==0:
+    exit()
+
+while len(a)<n:
+    a.append(int(input()))
+
+int(a[i])<=168 and int(a[i])>=0
+
+for a[i] in a:
+    if int(a[i])<=10:
+        print ("Go to work!")
+    elif int(a[i])>10 and int(a[i])<=25:
+        print ("You are weak")
+    elif int(a[i])>25 and int(a[i])<=45:
+        print ("Okay, fine")
+    elif int(a[i])>45:
+        print ("Burn! Burn! Burn Young!")
